e531c344ff28b79d2371afe27f3e8b4da141b367
Rodrigo Freund de Moraes <rodrigofreund@gmail.com>
2023-07-11 22:08:43 -0300

    adicionando exception

--- src/br/com/rodrigofreund/easyword/model/exception/BusinessException.java
+++ src/br/com/rodrigofreund/easyword/model/exception/BusinessException.java
@@ -2,8 +2,8 @@ package br.com.rodrigofreund.easyword.model.exception;
 
 public class BusinessException extends RuntimeException {
 
-    public BusinessException(String string) {
-        System.out.println(string);
+    public BusinessException(String message) {
+        super(message);
     }
 
     private static final long serialVersionUID = 1L;
